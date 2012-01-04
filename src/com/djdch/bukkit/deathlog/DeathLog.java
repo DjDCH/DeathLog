efffce851b1ab082c0c1e4bd515de4caf4cbda2c
DjDCH <bukkit.coding@djdch.com>
2012-01-04 15:35:26 -0500

    Fixed files for compliance of coding conventions.

--- src/com/djdch/bukkit/deathlog/DeathLog.java
+++ src/com/djdch/bukkit/deathlog/DeathLog.java
@@ -11,46 +11,46 @@ import com.djdch.bukkit.utils.Logger;
  * Main class of the <b>DeathLog</b> plugin for Bukkit.
  * <p>
  * Show death messages in the server console/log.
- *
+ * 
  * @author DjDCH
  */
 public class DeathLog extends JavaPlugin {
-	/**
-	 * Contains the Logger instance.
-	 */
-	protected final Logger logger = new Logger();
-	
-	/**
-	 * Contains the deathListener instance.
-	 */
-	protected final DeathListener deathListener = new DeathListener(this);
-	
-	/**
-	 * Method execute when the plugin is enable.
-	 */
-	public void onEnable() {
-		this.logger.setPrefix(getDescription().getName());
-		
-		// Register the plugin events
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvent(Event.Type.ENTITY_DEATH, deathListener, Event.Priority.Monitor, this);
-		
-		logger.info("Version " + getDescription().getVersion() + " enable.");
-	}
-	
-	/**
-	 * Method execute when the plugin is disable.
-	 */
-	public void onDisable() {
-		logger.info("Version " + getDescription().getVersion() + " disable.");
-	}
-	
-	/**
-	 * Accessor who return the logger instance.
-	 * 
-	 * @return Logger instance.
-	 */
-	public Logger getLogger() {
-		return this.logger;
-	}
+    /**
+     * Contains the Logger instance.
+     */
+    protected final Logger logger = new Logger();
+
+    /**
+     * Contains the deathListener instance.
+     */
+    protected final DeathListener deathListener = new DeathListener(this);
+
+    /**
+     * Method execute when the plugin is enable.
+     */
+    public void onEnable() {
+        this.logger.setPrefix(getDescription().getName());
+
+        // Register the plugin events
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.ENTITY_DEATH, deathListener, Event.Priority.Monitor, this);
+
+        logger.info("Version " + getDescription().getVersion() + " enable.");
+    }
+
+    /**
+     * Method execute when the plugin is disable.
+     */
+    public void onDisable() {
+        logger.info("Version " + getDescription().getVersion() + " disable.");
+    }
+
+    /**
+     * Accessor who return the logger instance.
+     * 
+     * @return Logger instance.
+     */
+    public Logger getLogger() {
+        return this.logger;
+    }
 }
